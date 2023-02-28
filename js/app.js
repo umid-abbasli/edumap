b32d0c166c71e737e28e261e527a56f970fb0d30
umudAbbasli <108527234+umudAbbasli@users.noreply.github.com>
2023-02-28 16:37:20 +0400

    changes

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
   // head
   $(".header-2-search").click(function () {
-    $(".header-search-section").slideToggle();
+    $(".header-search-section").slideToggle(200);
     $(".header-2-search > img").toggleClass("d-none");
     $(".header-2-search > i").toggleClass("d-none");
   });
@@ -35,42 +35,42 @@ $(document).ready(function () {
   // head end
 
   //   select country
-  $("#checkcountry").click(function () {
-    $(".select-contry-content").toggle();
-  });
-  $(".select-contry-content").click(function () {
-    $(".select-contry-content").hide();
-  });
+  // $("#checkcountry").click(function () {
+  //   $(".select-contry-content").toggle();
+  // });
+  // $(".select-contry-content").click(function () {
+  //   $(".select-contry-content").hide();
+  // });
 
   // select country end
 });
-let checkcountry = document.getElementById("checkcountry");
-window.addEventListener("mouseup", function (event) {
-  var pol = document.getElementById("select-contry-content");
-  if (event.target != pol && event.target.parentNode != pol) {
-    pol.style.display = "none";
-  }
-});
-let selectcontrycontent = document.getElementById("select-contry-content");
-const selectcountries ="azerbaijan +994 ,turkey +123 ,russian +007 ,ozbekistan +237 ,iran +785";
+// let checkcountry = document.getElementById("checkcountry");
+// window.addEventListener("mouseup", function (event) {
+//   var pol = document.getElementById("select-contry-content");
+//   if (event.target != pol && event.target.parentNode != pol) {
+//     pol.style.display = "none";
+//   }
+// });
+// let selectcontrycontent = document.getElementById("select-contry-content");
+// const selectcountries ="azerbaijan +994 ,turkey +123 ,russian +007 ,ozbekistan +237 ,iran +785";
 
-const selectcountriesarr = selectcountries.split(",");
+// const selectcountriesarr = selectcountries.split(",");
 
-let countryphone = "";
-for (let i = 0; i < selectcountriesarr.length; i++) {
-  countryphone += `<div onclick="addcountry('+${
-    selectcountriesarr[i].split("+")[1]
-  }')">
-  ${selectcountriesarr[i].split("+")[0]}<span>
-  +${
-    selectcountriesarr[i].split("+")[1]
-  }</span></div>`;
-}
-selectcontrycontent.innerHTML = countryphone;
+// let countryphone = "";
+// for (let i = 0; i < selectcountriesarr.length; i++) {
+//   countryphone += `<div onclick="addcountry('+${
+//     selectcountriesarr[i].split("+")[1]
+//   }')">
+//   ${selectcountriesarr[i].split("+")[0]}<span>
+//   +${
+//     selectcountriesarr[i].split("+")[1]
+//   }</span></div>`;
+// }
+// selectcontrycontent.innerHTML = countryphone;
 
-function addcountry(arq) {
-  checkcountry.innerHTML = arq;
-}
+// function addcountry(arq) {
+//   checkcountry.innerHTML = arq;
+// }
 // select country
 
 
